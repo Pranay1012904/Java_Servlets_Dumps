f452631e0480a43cb94b88a16e71359afffab5cc
pranayXoX <pranay.dmnsns@gmail.com>
2023-01-03 17:28:30 +0530

    Hyperlink Redirection applied!

---
+++ Redirection/src/main/java/com/test/one/Test1.java
@@ -0,0 +1,30 @@
+package com.test.one;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test1 extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//super.doGet(req, resp);
+		resp.setContentType("text/html");
+		PrintWriter out=resp.getWriter();
+		
+		out.print("<html>");
+		out.print("<head>");
+		out.print("</head>");
+		out.print("<body>");
+		out.print("<h4>Ridirect to another servlet</h4>");
+		out.print("<a href='http://localhost:8090/Redirection/serv2'><button>Click Here</button></a>");
+		out.print("</body>");
+		out.print("</html>");
+	}
+
+}
---
+++ Redirection/src/main/java/com/test/one/Test2.java
@@ -0,0 +1,29 @@
+package com.test.one;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test2 extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//super.doGet(req, resp);
+		resp.setContentType("text/html");
+		PrintWriter out=resp.getWriter();
+		
+		out.print("<html>");
+		out.print("<head>");
+		out.print("</head>");
+		out.print("<body>");
+		out.print("<h4>Ridirected servlet opened!!</h4>");
+		
+		out.print("</body>");
+		out.print("</html>");
+	}
+
+}
